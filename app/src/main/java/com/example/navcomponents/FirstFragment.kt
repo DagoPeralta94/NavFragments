55c4fd1d13a6b3047d4b3b328f45a4282525e870
DagoPeralta94 <dagoperalta163@gmail.com>
2022-10-26 09:50:54 -0500

    Add Binding and nav parameters

--- app/src/main/java/com/example/navcomponents/FirstFragment.kt
+++ app/src/main/java/com/example/navcomponents/FirstFragment.kt
@@ -6,17 +6,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.findFragment
 import androidx.navigation.fragment.findNavController
+import com.example.navcomponents.databinding.FragmentFirstBinding
 
-class FirstFragment : Fragment(R.layout.fragment_first) {
+class FirstFragment : Fragment() {
+
+    private var _binding : FragmentFirstBinding? = null
+    private val binding get()  = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val button = view.findViewById<Button>(R.id.bt_nav)
 
-        button.setOnClickListener {
-            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
-            onDestroyView()
+        binding.btNav.setOnClickListener {
+            val getName = binding.etName.text.toString()
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(getName)
+            findNavController().navigate(action)
         }
     }
 }
--- app/src/main/java/com/example/navcomponents/SecondFragment.kt
+++ app/src/main/java/com/example/navcomponents/SecondFragment.kt
@@ -6,15 +6,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.navcomponents.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment(R.layout.fragment_second) {
 
+    val args: SecondFragmentArgs by navArgs()
+    private var _binding : FragmentSecondBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val button = view.findViewById<Button>(R.id.bt_nav2)
+        val name = args.name
+        binding.txtName.text = name
 
-        button.setOnClickListener {
+        binding.btNav2.setOnClickListener {
             findNavController().navigate(R.id.action_secondFragment_to_thirdFragment)
             onDestroyView()
         }
